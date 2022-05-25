7e05a56dc4b5ad7fdbc16fe2bd03f5bfbeec2323
jmanuares <jmanuares@gmail.com>
2022-05-26 00:11:55 +0200

    empiezo el proceso de descarga

--- Program/index.js
+++ Program/index.js
@@ -4,9 +4,11 @@ const path = require('path').resolve(__dirname, '../Program/');
 
 const tracker = require(path + '/src/tracker.js');
 const torrentParser = require(path + '/src/torrentParser.js');
+const download = require(path + '/src/download.js');
 
 
-const torrent = torrentParser.open(path+'/puppy.torrent');
+
+const torrent = torrentParser.open(process.argv[2]);
 
 tracker.getPeers(torrent, peers => {
     console.log('list of peers: ', peers);
